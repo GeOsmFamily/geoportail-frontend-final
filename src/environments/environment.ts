9cc01e51d924754a9f658ebb10c2896e1190028c
BorisGautier <tchoukouahaboris@outlook.fr>
2022-01-31 11:59:07 +0100

    update project environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  global_logo: undefined,
+  primaryColor: '#023f5f',
+  url_prefix: 'url_backend/',
+  url_frontend: 'urlFrontend/',
+  url_service: 'https://service.geo.sm/',
+  path_qgis: '/var/www/geosm/',
+  projet_nodejs: 'pojet_nodejs_value',
+  nom: 'nomInstance',
+  countrycode: 'code_country',
+  avaible_language: ['fr', 'en', 'es'],
+  default_language: 'langue',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
